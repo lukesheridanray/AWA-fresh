7531c139c5663f4ccbe4871f7969cb95770acbc1
Jamie Woolgar <jamie@castlegateit.co.uk>
2014-09-24 12:04:05 +0100

    TTS VAT qualifier update

--- tts-group/vat-experiment/vat-experiment-ex-vat.js
+++ tts-group/vat-experiment/vat-experiment-ex-vat.js
@@ -19,15 +19,24 @@ console.log('VAT experiment - ex VAT 0.3');
 // Variables
 // Object containing variables, generally these would be strings or jQuery objects
 exp.vars = {
-    'allPrices': $('.rr-price a, #product_price, .product_table h2'),
+    'allPrices': $('.rr-price a, #product_price, .product_table h2, .product_range_holder h3'),
     'allVatPrices': $('#product_price_inc_vat, .was_price_height'),
     'basketPrices': $('.main_basket td.price,.main_basket td.total'),
-    'basketWidget': $('.header_basket')
+    'basketWidget': $('.header_basket'),
+    'wasPrices': $('.was_price')
 };
 
 // Styles
 // String containing the CSS for the experiment
 exp.css = ' \
+.product_range_holder { height: 496px; } \
+.product_range_holder .buy_button { top: 55px; } \
+.product_range_holder h4 { padding-left: 10px; clear: left; } \
+.product_range_holder h4.was_price { top: 320px; left: 0; } \
+.product_table h2 .small_label { display: block; padding-left: 0; } \
+.product_table h3.alt_head { padding-top: 20px; } \
+.product_table h2 .small_label { position: relative; top: -8px; } \
+.product_table h2#product_price .small_label { top: 0; } \
 .plus-x-vat { color: #666; font-size: 0.9em; font-weight: normal; } \
 .main_basket .product_pricing .total { width: 150px; } \
 .main_basket .product_pricing .price { width: 150px; }';
@@ -42,11 +51,20 @@ exp.func.modPrices = function( el, vat ) {
     var percentage = 0.2;
     el.each(function(){
         var self = $(this);
-        var parts = self.html().match(/(.*)(£)(.*)(<span)(.*)/i);
-        var priceBase = Math.round( parseFloat(parts[3]) * 100) / 100;
-        var priceResult = (priceBase * percentage).toFixed(2);
-        self.html( parts[1] + parts[2] + parts[3] + parts[4] + parts[5].replace('ex VAT', 'plus &pound;' + priceResult + ' VAT' ) );
-        //self.next('h3').html( 'plus &pound;' + priceResult + ' VAT' );
+        var parts;
+        var priceBase;
+        var priceResult;
+        if ( self.parents('.product_range_holder').length ) {
+            parts = self.html().match(/(.*)(£)(.*)/i);
+            priceBase = Math.round( parseFloat(parts[3]) * 100) / 100;
+            priceResult = (priceBase * percentage).toFixed(2);
+            self.next('h4').html( 'plus &pound;' + priceResult + ' VAT' );
+        } else {
+            parts = self.html().match(/(.*)(£)(.*)(<span)(.*)/i);
+            priceBase = Math.round( parseFloat(parts[3]) * 100) / 100;
+            priceResult = (priceBase * percentage).toFixed(2);
+            self.html( parts[1] + parts[2] + parts[3] + parts[4] + parts[5].replace('ex VAT', 'plus &pound;' + priceResult + ' VAT' ) );
+        }
     });
 };
 
@@ -78,6 +96,16 @@ exp.init = function() {
     if( this.vars.allPrices.length ) {
         // modify the ex VAT prices to inc VAT
         exp.func.modPrices( this.vars.allPrices, 'ex' );
+    }
+    
+    if( this.vars.wasPrices.length ) {
+        // add ex vat to was prices to be absolutely clear
+        this.vars.wasPrices.each(function(){
+            var self = $(this);
+            if( self.html().indexOf('£') !== -1 ) {
+                self.append( ' ex VAT' );      
+            }
+        });
     }
 
     if( this.vars.allVatPrices.length ) {
