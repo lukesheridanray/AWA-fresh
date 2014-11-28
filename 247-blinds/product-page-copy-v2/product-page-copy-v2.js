b7e85c7d008b2767e48d3eac2bf63106689eaf64
Dániel Kávássy <daniel@castlegateit.co.uk>
2014-11-28 15:52:03 +0000

    247 blinds update

--- 247-blinds/product-page-copy-v2/product-page-copy-v2.js
+++ 247-blinds/product-page-copy-v2/product-page-copy-v2.js
@@ -23,7 +23,7 @@ console.log('Product Page Copy 2 - 0.1');
 
 // Condition
 // If we cannot rely on URL's to target the experiment (always preferred), we can use a unique CSS selector
-exp.condition = $('.unique-selector');
+exp.condition = $('#ac-description > .panel-body');
 
 // Check for a condition and return false if it has not been met
 if(exp.condition && !exp.condition.length) {
@@ -36,44 +36,43 @@ if(exp.condition && !exp.condition.length) {
 exp.vars = {
     'copy': {
         'http://www.247blinds.co.uk/ecowood-pure-white-venetian-blind':
-        '<p>Bring a touch of New England chic into your home, with a solid Ecowood Venetian blind in Pure White, a matt white colour. This sophisticated blind is made to order to fit your window perfectly and normally arrives in around a week.</p>
+        '<p>Bring a touch of New England chic into your home, with a solid Ecowood Venetian blind in Pure White, a matt white colour. This sophisticated blind is made to order to fit your window perfectly and normally arrives in around a week.</p> \
         <p><p>All our Ecowood Venetian blinds come ready to fit with a robust, co-ordinating headrail, woven control cords and all fixings. Not sure about colour or style? Ask for a free sample before you order</p>',
 
         'http://www.247blinds.co.uk/ecowood-barley-venetian-blind':
-        '<p>Bring a touch of New England chic into your home, with a solid Ecowood Venetian blind in Barley, a rich, natural wood colour. This sophisticated blind is made to order to fit your window perfectly and normally arrives in around a week. </p>
+        '<p>Bring a touch of New England chic into your home, with a solid Ecowood Venetian blind in Barley, a rich, natural wood colour. This sophisticated blind is made to order to fit your window perfectly and normally arrives in around a week. </p> \
         <p>All our Ecowood Venetian blinds come ready to fit with a robust, co-ordinating headrail, woven control cords and all fixings. Not sure about colour or style? Ask for a free sample before you order</p>',
 
         'http://www.247blinds.co.uk/ecowood-deluxe-pure-white-venetian-blind':
-        '<p>Bring a touch of New England chic into your home, with a solid Ecowood Deluxe Venetian blind in Pure White, a matt white colour. The deluxe version of this blind features vertical ladder tapes for a designer look. It is made to order to fit your window perfectly and normally arrives in around a week. </p>
+        '<p>Bring a touch of New England chic into your home, with a solid Ecowood Deluxe Venetian blind in Pure White, a matt white colour. The deluxe version of this blind features vertical ladder tapes for a designer look. It is made to order to fit your window perfectly and normally arrives in around a week. </p> \
         <p>All our deluxe Ecowood Venetian blinds come ready to fit with a robust, co-ordinating headrail, woven control cords and all fixings. Not sure about colour or style? Ask for a free sample before you order</p>',
 
-         
         'http://www.247blinds.co.uk/ecowood-cotton-venetian-blind':
-        '<p>Bring a touch of New England chic into your home, with a solid Ecowood Venetian blind in Cotton, a gentle, off-white colour with a matt finish.This sophisticated blind is made to order to fit your window perfectly and normally arrives in around a week. </p>
+        '<p>Bring a touch of New England chic into your home, with a solid Ecowood Venetian blind in Cotton, a gentle, off-white colour with a matt finish.This sophisticated blind is made to order to fit your window perfectly and normally arrives in around a week. </p> \
         <p>All our Ecowood Venetian blinds come ready to fit with a robust, co-ordinating headrail, woven control cords and all fixings. Not sure about colour or style? Ask for a free sample before you order</p>',
 
         'http://www.247blinds.co.uk/ecowood-cream-venetian-blind':
-        '<p>Bring a touch of New England chic into your home, with a solid Ecowood Venetian blind in Cream, a pale, off-white colour with a matt finish. This sophisticated blind is made to order to fit your window perfectly and normally arrives in around a week. </p>
+        '<p>Bring a touch of New England chic into your home, with a solid Ecowood Venetian blind in Cream, a pale, off-white colour with a matt finish. This sophisticated blind is made to order to fit your window perfectly and normally arrives in around a week. </p> \
         <p>All our Ecowood Venetian blinds come ready to fit with a robust, co-ordinating headrail, woven control cords and all fixings. Not sure about colour or style? Ask for a free sample before you order</p>',
 
         'http://www.247blinds.co.uk/origin-basic-white-venetian-blind':
-        '<p>Venetian blinds are a timeless look that suits most rooms and give you excellent privacy as well as total control of light. This Venetian Blind in Basic White is from our Origin collection. It is made to order to fit your window perfectly and normally arrives in around a week. </p>
+        '<p>Venetian blinds are a timeless look that suits most rooms and give you excellent privacy as well as total control of light. This Venetian Blind in Basic White is from our Origin collection. It is made to order to fit your window perfectly and normally arrives in around a week. </p> \
         <p>All our Origin Venetian blinds have high quality fittings to ensure your blind opens and closes smoothly and come ready to fit with all fixings included. Not sure about colour or style? Ask for a free sample before you order</p>',
 
         'http://www.247blinds.co.uk/origin-brushed-silver-venetian-blind':
-        '<p>Venetian blinds are a timeless look that suits most rooms and give you excellent privacy as well as total control of light. This Venetian Blind in a cool brushed silver finish is from our Origin collection. It is made to order to fit your window perfectly and normally arrives in around a week. </p>
+        '<p>Venetian blinds are a timeless look that suits most rooms and give you excellent privacy as well as total control of light. This Venetian Blind in a cool brushed silver finish is from our Origin collection. It is made to order to fit your window perfectly and normally arrives in around a week. </p> \
         <p>All our Origin Venetian blinds have high quality fittings to ensure your blind opens and closes smoothly and come ready to fit with all fixings included. Not sure about colour or style? Ask for a free sample before you order.</p>',
 
         'http://www.247blinds.co.uk/rimini-white-roller-blind':
-        '<p>Smart, neat, and practical, the clean lines of a roller blind suit any room. Made from a high quality plain fabric in a white colour, this roller blind from our Rimini collection is made to order for your window and normally arrives in around a week. </p>
+        '<p>Smart, neat, and practical, the clean lines of a roller blind suit any room. Made from a high quality plain fabric in a white colour, this roller blind from our Rimini collection is made to order for your window and normally arrives in around a week. </p> \
         <p>When ordering, please note that the roller mechanism means that the cloth of your blind is always 3-4cm narrower than the overall width. See measuring guide below for more details. Not sure about colour, texture or pattern? Ask for a free sample before you order.  </p>',
 
         'http://www.247blinds.co.uk/ajanta-raven-roller-blind':
-        '<p>Smart, neat, and practical, the clean lines of a roller blind suit any room. Made from a striking ‘crackle-effect’ fabric, this blackout roller blind from our Carmela collection is made to order for your window and normally arrives in around a week. </p>
+        '<p>Smart, neat, and practical, the clean lines of a roller blind suit any room. Made from a striking ‘crackle-effect’ fabric, this blackout roller blind from our Carmela collection is made to order for your window and normally arrives in around a week. </p> \
         <p>When ordering, please note that the roller mechanism means that the cloth of your blind is always 3-4cm narrower than the overall width. See measuring guide below for more details. Not sure about colour, texture or pattern? Ask for a free sample before you order.  </p>',
 
         'http://www.247blinds.co.uk/rimini-white-vertical-blind':
-        '<p>Add height and elegance to your room with a Vertical Blind. Louvres are made high quality fabric in a plain white colour, and have weighted hems. This contemporary style blind from our Rimini collection is made to order to fit your window perfectly and normally arrives in less than a week.</p>
+        '<p>Add height and elegance to your room with a Vertical Blind. Louvres are made high quality fabric in a plain white colour, and have weighted hems. This contemporary style blind from our Rimini collection is made to order to fit your window perfectly and normally arrives in less than a week.</p> \
         <p>All our Vertical Blinds come ready to fit with a slimline, Easyglide headrail, matching draw rod to tilt and open the vanes and all the fixings you need. Not sure about colour, texture or pattern? Ask for a free sample before you order.  </p>'
     }
 };
@@ -137,19 +136,12 @@ exp.init = function() {
 
     // Some example DOM manipulation...
 
-    $('.header').append(this.vars.productDesc);
-
-    if(this.vars.productSku) {
-        $('#tagLine').html(this.vars.productSku);
+    // Update copy
+    var page_url = window.location.href.match(/[^?]+/)[0];
+    var copy = exp.vars.copy[page_url];
+    if (copy) {
+        $('#ac-description > .panel-body').html(copy);
     }
-
-    this.func.waitForFunction( openModal, function myCallback() {
-        openModal('modal content');
-    });
-
-    this.func.waitForElement( '.reviews-widget', function anotherCallback() {
-        $('.reviews-widget').append('Appending some content to the widget');
-    });
 
 };
 
