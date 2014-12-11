5b69c0d524149c2f528e80f5d75f261664d104c2
Jamie Dust <jamie@jamiewoolgar.net>
2014-12-11 16:53:32 +0000

    Book People product page / price per book

--- the-book-people/price-per-book/price-per-book.js
+++ the-book-people/price-per-book/price-per-book.js
@@ -30,7 +30,7 @@ exp.vars = {
     variation: 3,
     isCollection: ( $('#format-label').text().indexOf('Collection') !== -1 ) ? true : false,
     totalPrice: $('.priceWrapper #price .price.price-string').text().trim(),
-    books: $('#product-description-title').text().match(/( - )(.*)( Books)/i)[2]
+    books: $('#product-description-title').text().match(/([0-9]*)( Books)/i)
 };
 
 // Styles
@@ -47,7 +47,7 @@ exp.css = ' \
 #saving { \
     font-size: 1.1em; \
     margin: 5px 0 -5px 0; \
-    font-weight: bold;
+    font-weight: bold; \
 } \
 .exp-savings-value { \
     font-size: 1.2em; \
@@ -72,52 +72,148 @@ exp.css = ' \
     display: block; \
     float: left; \
 } \
+.productImage { \
+    position: relative; \
+} \
+.exp-price-per-book--red-circle { \
+    background: #CD0001; \
+    width: 106px; \
+    border-radius: 106px; \
+    position: absolute; \
+    bottom: 21px; \
+    left: 11px; \
+    color: #fff; \
+    font-weight: bold; \
+} \
+@media screen and (max-width: 1199px) { \
+    .exp-price-per-book--red-circle { \
+        left: 8px; \
+        bottom: 19px; \
+        width: 86px; \
+        padding-left: 2px !important; \
+        font-size: 1em !important; \
+    } \
+} \
+/* Variation 1 */ \
+.exp-variation-1 .exp-price-per-book--red-circle { \
+    font-size: 1.4em; \
+    padding: 32px 0 0 0; \
+    height: 74px; \
+} \
+.exp-variation-1 .exp-price-per-book--red-circle .ppb-pence { \
+    font-size: 1.3em; \
+} \
+.exp-variation-1 .exp-price-per-book--red-circle .ppb-pounds { \
+    font-size: 1.25em; \
+} \
+@media screen and (max-width: 1199px) { \
+    .exp-variation-2 .exp-price-per-book--red-circle { \
+        padding: 24px 0 0 0; \
+        height: 64px; \
+    } \
+} \
+/* Variation 2 */ \
+.exp-variation-2 .exp-price-per-book--red-circle { \
+    font-size: 1.25em; \
+    padding: 0; \
+    height: 106px; \
+    line-height: 1.3em; \
+} \
+.exp-variation-2 .exp-price-per-book--red-circle strong { \
+    display: block; \
+    padding-bottom: 11px; \
+    padding: 10px 0 6px 0; \
+    line-height: 1.1em; \
+} \
+.exp-variation-2 .exp-price-per-book--red-circle strong em { \
+    font-style: normal; \
+    font-size: 1.25em; \
+} \
+.exp-variation-2 .exp-price-per-book--red-circle .ppb-pence { \
+    font-size: 1.3em; \
+} \
+.exp-variation-2 .exp-price-per-book--red-circle .ppb-pounds { \
+    font-size: 1.25em; \
+} \
+@media screen and (max-width: 1199px) { \
+    .exp-variation-2 .exp-price-per-book--red-circle { \
+        height: 88px; \
+    } \
+} \
 /* Variation 3 */ \
 .exp-variation-3 #rrp { \
     position: absolute; \
     top: 1px; \
     right: -5px; \
-}';
+} \
+.exp-price-per-book--price-box { \
+    margin: 4px 0 2px 0; \
+} \
+@media screen and (max-width: 1199px) { \
+    .exp-variation-3 #rrp { \
+        top: 20px; \
+        left: 0; \
+    } \
+    .exp-price-per-book--price-box { \
+        padding-top: 22px; \
+    } \
+} ';
 
 // Functions
 // Object containing functions, some helpful functions are included
 exp.func = {};
 
+exp.func.roundUp = function(number, digits){
+    var factor = Math.pow(10,digits);
+    return (Math.ceil(number*factor) / factor).toFixed(2);
+};
+
 exp.func.getPricePerBook = function(variation) {
-    var variation = variation || false;
+    variation = variation || false;
     var total = parseFloat( exp.vars.totalPrice );
-    var books = parseFloat( exp.vars.books );
+    var books = parseFloat( exp.vars.books[1] );
     var priceString;
-    var pricePerBook = (total / books).toFixed(2);
+    var pricePerBook = exp.func.roundUp( (total / books), 2 );
 
-
-    if( pricePerBook < 1 ) {
-        pricePerBook = pricePerBook.replace('/(.*)(/.)(.*)/',function(match,p1,p2){
-            return p1 + 'p';
-        });
-    } else {
-        pricePerBook = '£' + pricePerBook;
-    }
+exp.log( 'toFixed: ' + (total / books).toFixed(2) + ' - roundUp: ' + pricePerBook );
 
     if( variation === false || pricePerBook > 9.99) {
         return false;
     }
 
+    if( parseFloat( pricePerBook ) < 1 ) {
+
+        pricePerBook = pricePerBook.match(/([0-9]*)(.)([0-9]*)/)[3] + 'p';
+
+    } else {
+
+        pricePerBook = ( '£' + pricePerBook ).replace('.00', '');
+
+    }
+
     if( variation === 1 ) {
 
-        return;
-
+        priceString = 'Just ' + ( (pricePerBook.indexOf('p') !== -1) ? '<span class="ppb-pence">'+pricePerBook+'</span>' : '<span class="ppb-pounds">'+pricePerBook+'</span>' ) + '<br />per book!';
+    
     } else if( variation === 2 ) {
+       
+        priceString = '<strong><em>' + books + '</em><br />books</strong>Just ' + ( (pricePerBook.indexOf('p') !== -1) ? '<span class="ppb-pence">'+pricePerBook+'</span>' : '<span class="ppb-pounds">'+pricePerBook+'</span>' ) + '<br />per book!';
+    
+    }
 
+    if( variation === 1 || variation === 2 ) {
+        $('.span3 .productImage:eq(0)').append(
+            '<div class="exp-price-per-book--red-circle">' +
+            priceString +
+            '</div>'
+        );
     } else if( variation === 3 ) {
-
         priceString = 'ONLY ' + pricePerBook + ' per book';
         $('.exp-savings-label').before(
             '<div class="exp-price-per-book--price-box">' +
                 priceString +
             '</div>'
         );
-
     }
 
 };
@@ -174,10 +270,9 @@ exp.init = function() {
     */
 
     // Add variation class to body
-
     $('body').addClass('exp-variation-' + this.vars.variation );
 
-    // Click handler for review start scrollto
+    // Click handler for review stars scrollto
     $('#average-review-stars').bind('click', function(){
         var $target = $('#review-section');
         $('html,body').animate({
