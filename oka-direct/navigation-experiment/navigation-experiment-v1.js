18637122f0b65a113a78ee0ccf9dee80fc2aa101
Jamie Woolgar <jamie@castlegateit.co.uk>
2015-03-30 22:22:18 +0100

    OKA Direct, nav experiment adjustments

--- oka-direct/navigation-experiment/navigation-experiment-v1.js
+++ oka-direct/navigation-experiment/navigation-experiment-v1.js
@@ -2,6 +2,8 @@
 // a proof of concept for AWA
 // removing 3 items from the menu
 
+// Desktop / tablet
+
 // remove Gifts
 $('#cat-nav .custom-touch2').filter(
     function(){
@@ -18,3 +20,16 @@ $('#cat-nav .custom-touch2').filter(
         return $(this).find('a:eq(0)').text().trim().indexOf('New Season') !== -1;
     }
 ).addClass('last-child');
+
+// Mobile
+
+// Remove menu items
+$('.mobile-nav li:contains("Gifts"), \
+    .mobile-nav li:contains("Online Exclusives"), \
+    .mobile-nav li:contains("OKA Blog")'
+).remove();
+
+// Log to console if available
+if (typeof window.console !== 'undefined') {
+    console.log('Navigation experiment v1');
+}
