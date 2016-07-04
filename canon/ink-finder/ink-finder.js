ee1fb53d8c1ad65a664d0292dc5b575941a5ea86
Jamie Woolgar <jamie@castlegateit.co.uk>
2016-07-04 15:03:01 +0100

    Canon - Ink finder - amendments as per Canons feedback

--- canon/ink-finder/ink-finder.js
+++ canon/ink-finder/ink-finder.js
@@ -33,14 +33,14 @@
                 title: 'Can we help you choose your Ink & Paper?',
 
                 intro: {
-                    cat: 'Give us the range and model for your printer and w&acute;ll,\
-                        find you the compatible ink & paper for your printer',
+                    cat: 'Give us the range and model for your printer and we\'ll, \
+                        find you the compatible ink & paper for your printer.',
                     listings: 'Find ink for my printer'
                 },
 
                 button: {
                     cat: 'Find ink for my printer',
-                    listings: 'Find my ink now'
+                    listings: 'Find my ink now &raquo;'
                 },
 
                 range: 'My printer range',
@@ -77,109 +77,103 @@
             Other: '/ink-toner/f/3349056800/'
         },
         'PIXMA MG series': {
-/*
-MG2100 series   PIXMA MG2150    /pixma-mg2150-ink-cartridges
-MG2200 series   PIXMA MG2250    /pixma-mg2250-ink-cartridges
-MG2400 series   PIXMA MG2450    /pixma-mg2450-ink-cartridges
-MG2500 series   PIXMA MG2550    /pixma-mg2550-ink-cartridges
-MG2900 series   PIXMA MG2950    /pixma-mg2950-ink-cartridges
-MG3100 series   PIXMA MG3150    /pixma-mg3150-ink-cartridges
-MG3200 series   PIXMA MG3250    /pixma-mg3250-ink-cartridges
-MG3500 series   PIXMA MG3550    /pixma-mg3550-ink-cartridges
-MG3600 series   PIXMA MG3650    /pixma-mg3650-ink-cartridges
-MG4100 series   PIXMA MG4150    /pixma-mg4150-ink-cartridges
-MG4200 series   PIXMA MG4250    /pixma-mg4250-ink-cartridges
-MG5100 series   PIXMA MG5150    /pixma-mg5150-ink-cartridges
-MG5200 series   PIXMA MG5250    /pixma-mg5250-ink-cartridges
-MG5300 series   PIXMA MG5350    /pixma-mg5350-ink-cartridges
-MG5400 series   PIXMA MG5450    /pixma-mg5450-ink-cartridges
-MG5500 series   PIXMA MG5550    /pixma-mg5550-ink-cartridges
-    MG5650  /pixma-mg5650-ink-cartridges
-MG5700 series   PIXMA MG5750    /pixma-mg5750-ink-cartridges
-    PIXMA MG5751    /pixma-mg5751-ink-cartridges
-    PIXMA MG5752    /pixma-mg5752-ink-cartridges
-    PIXMA MG5753    /pixma-mg5753-ink-cartridges
-MG6100 series   PIXMA MG6150    /pixma-mg6150-ink-cartridges
-MG6200 series   PIXMA MG6250    /pixma-mg6250-ink-cartridges
-MG6300 series   PIXMA MG6350    /pixma-mg6350-ink-cartridges
-MG6400 series   PIXMA MG6450    /pixma-mg6450-ink-cartridges
-MG6600 series   PIXMA MG6650    /pixma-mg6650-ink-cartridges
-MG6800 series   PIXMA MG6850    /pixma-mg6850-ink-cartridges
-    PIXMA MG6851    /pixma-mg6851-ink-cartridges
-    PIXMA MG6852    /pixma-mg6852-ink-cartridges
-    PIXMA MG6853    /pixma-mg6853-ink-cartridges
-MG7100 series   PIXMA MG7150    /pixma-mg7150-ink-cartridges
-MG7500 series   PIXMA MG7550    /pixma-mg7550-ink-cartridges
-MG7700 series   PIXMA MG7750    /pixma-mg7750-ink-cartridges
-                PIXMA MG7751    /pixma-mg7751-ink-cartridges
-                PIXMA MG7752    /pixma-mg7752-ink-cartridges
-                PIXMA MG7753    /pixma-mg7753-ink-cartridges
-MG8100 series   PIXMA MG8150    /pixma-mg8150-ink-cartridges
-MG8200 series   PIXMA MG8250    /pixma-mg8250-ink-cartridges
-*/
+            MG2150: '/pixma-mg2150-ink-cartridges',
+            MG2250: '/pixma-mg2250-ink-cartridges',
+            MG2450: '/pixma-mg2450-ink-cartridges',
+            MG2550: '/pixma-mg2550-ink-cartridges',
+            MG2950: '/pixma-mg2950-ink-cartridges',
+            MG3150: '/pixma-mg3150-ink-cartridges',
+            MG3250: '/pixma-mg3250-ink-cartridges',
+            MG3550: '/pixma-mg3550-ink-cartridges',
+            MG3650: '/pixma-mg3650-ink-cartridges',
+            MG4150: '/pixma-mg4150-ink-cartridges',
+            MG4250: '/pixma-mg4250-ink-cartridges',
+            MG5150: '/pixma-mg5150-ink-cartridges',
+            MG5250: '/pixma-mg5250-ink-cartridges',
+            MG5350: '/pixma-mg5350-ink-cartridges',
+            MG5450: '/pixma-mg5450-ink-cartridges',
+            MG5550: '/pixma-mg5550-ink-cartridges',
+            MG5650: '/pixma-mg5650-ink-cartridges',
+            MG5750: '/pixma-mg5750-ink-cartridges',
+            MG5751: '/pixma-mg5751-ink-cartridges',
+            MG5752: '/pixma-mg5752-ink-cartridges',
+            MG5753: '/pixma-mg5753-ink-cartridges',
+            MG6150: '/pixma-mg6150-ink-cartridges',
+            MG6250: '/pixma-mg6250-ink-cartridges',
+            MG6350: '/pixma-mg6350-ink-cartridges',
+            MG6450: '/pixma-mg6450-ink-cartridges',
+            MG6650: '/pixma-mg6650-ink-cartridges',
+            MG6850: '/pixma-mg6850-ink-cartridges',
+            MG6851: '/pixma-mg6851-ink-cartridges',
+            MG6852: '/pixma-mg6852-ink-cartridges',
+            MG6853: '/pixma-mg6853-ink-cartridges',
+            MG7150: '/pixma-mg7150-ink-cartridges',
+            MG7550: '/pixma-mg7550-ink-cartridges',
+            MG7750: '/pixma-mg7750-ink-cartridges',
+            MG7751: '/pixma-mg7751-ink-cartridges',
+            MG7752: '/pixma-mg7752-ink-cartridges',
+            MG7753: '/pixma-mg7753-ink-cartridges',
+            MG8150: '/pixma-mg8150-ink-cartridges',
+            MG8250: '/pixma-mg8250-ink-cartridges',
             Other: '/ink-toner/f/3349056800/'
         },
         'PIXMA MP series': {
-/*
-            MP190 /pixma-mp190-ink-cartridges
-            MP210    /pixma-mp210-ink-cartridges
-            MP220    /pixma-mp220-ink-cartridges
-            MP230     /pixma-mp230-ink-cartridges
-            MP240 /pixma-mp240-ink-cartridges
-            MP250 /pixma-mp250-ink-cartridges
-            MP252 /pixma-mp252-ink-cartridges
-            MP260 /pixma-mp260-ink-cartridges
-            MP270 /pixma-mp270-ink-cartridges
-            MP272 /pixma-mp272-ink-cartridges
-            MP280     /pixma-mp280-ink-cartridges
-            MP282 /pixma-mp282-ink-cartridges
-            MP480 /pixma-mp480-ink-cartridges
-            MP482 /pixma-mp482-ink-cartridges
-            MP490 /pixma-mp490-ink-cartridges
-            MP492 /pixma-mp492-ink-cartridges
-            MP495     /pixma-mp495-ink-cartridges
-            MP499     /pixma-mp499-ink-cartridges
-            MP520    /pixma-mp520-ink-cartridges
-            MP540 /pixma-mp540-ink-cartridges
-            MP550 /pixma-mp550-ink-cartridges
-            MP560 /pixma-mp560-ink-cartridges
-            MP610    /pixma-mp610-ink-cartridges
-            MP620 /pixma-mp620-ink-cartridges
-            MP630 /pixma-mp630-ink-cartridges
-            MP640 /pixma-mp640-ink-cartridges
-            MP980 /pixma-mp980-ink-cartridges
-            MP990 /pixma-mp990-ink-cartridges
-*/
+            MP190: '/pixma-mp190-ink-cartridges',
+            MP210: '/pixma-mp210-ink-cartridges',
+            MP220: '/pixma-mp220-ink-cartridges',
+            MP230: '/pixma-mp230-ink-cartridges',
+            MP240: '/pixma-mp240-ink-cartridges',
+            MP250: '/pixma-mp250-ink-cartridges',
+            MP252: '/pixma-mp252-ink-cartridges',
+            MP260: '/pixma-mp260-ink-cartridges',
+            MP270: '/pixma-mp270-ink-cartridges',
+            MP272: '/pixma-mp272-ink-cartridges',
+            MP280: '/pixma-mp280-ink-cartridges',
+            MP282: '/pixma-mp282-ink-cartridges',
+            MP480: '/pixma-mp480-ink-cartridges',
+            MP482: '/pixma-mp482-ink-cartridges',
+            MP490: '/pixma-mp490-ink-cartridges',
+            MP492: '/pixma-mp492-ink-cartridges',
+            MP495: '/pixma-mp495-ink-cartridges',
+            MP499: '/pixma-mp499-ink-cartridges',
+            MP520: '/pixma-mp520-ink-cartridges',
+            MP540: '/pixma-mp540-ink-cartridges',
+            MP550: '/pixma-mp550-ink-cartridges',
+            MP560: '/pixma-mp560-ink-cartridges',
+            MP610: '/pixma-mp610-ink-cartridges',
+            MP620: '/pixma-mp620-ink-cartridges',
+            MP630: '/pixma-mp630-ink-cartridges',
+            MP640: '/pixma-mp640-ink-cartridges',
+            MP980: '/pixma-mp980-ink-cartridges',
+            MP990: '/pixma-mp990-ink-cartridges',
             Other: '/ink-toner/f/3349056800/'
         },
         'PIXMA MX series': {
-/*
-PIXMA MX300 PIXMA MX300 /pixma-mx300-ink-cartridges
-MX320 series    PIXMA MX320     /pixma-mx320-ink-cartridges
-MX330 series    PIXMA MX330     /pixma-mx330-ink-cartridges
-MX340 series    PIXMA MX340     /pixma-mx340-ink-cartridges
-MX350 series    PIXMA MX350     /pixma-mx350-ink-cartridges
-MX360 series    PIXMA MX360     /pixma-mx360-ink-cartridges
-MX370 series    PIXMA MX375 /pixma-mx375-ink-cartridges
-MX390 series    PIXMA MX395 /pixma-mx395-ink-cartridges
-MX410 series    PIXMA MX410     /pixma-mx410-ink-cartridges
-MX420 series    PIXMA MX420     /pixma-mx420-ink-cartridges
-MX430 series    PIXMA MX435 /pixma-mx435-ink-cartridges
-MX450 series    PIXMA MX455 /pixma-mx455-ink-cartridges
-MX470 series    PIXMA MX475 /pixma-mx475-ink-cartridges
-MX490 series    PIXMA MX495 /pixma-mx495-ink-cartridges
-MX510 series    PIXMA MX515 /pixma-mx515-ink-cartridges
-MX520 series    PIXMA MX525 /pixma-mx525-ink-cartridges
-MX530 series    PIXMA MX535 /pixma-mx535-ink-cartridges
-PIXMA MX700 PIXMA MX700 /pixma-mx700-ink-cartridges
-MX710 series    PIXMA MX715 /pixma-mx715-ink-cartridges
-MX720 series    PIXMA MX725 /pixma-mx725-ink-cartridges
-MX860 series    PIXMA MX860     /pixma-mx860-ink-cartridges
-MX870 series    PIXMA MX870     /pixma-mx870-ink-cartridges
-MX880 series    PIXMA MX885 /pixma-mx885-ink-cartridges
-MX890 series    PIXMA MX895 /pixma-mx895-ink-cartridges
-MX920 series    PIXMA MX925 /pixma-mx925-ink-cartridges
-*/
+            MX300: '/pixma-mx300-ink-cartridges',
+            MX320: '/pixma-mx320-ink-cartridges',
+            MX330: '/pixma-mx330-ink-cartridges',
+            MX340: '/pixma-mx340-ink-cartridges',
+            MX350: '/pixma-mx350-ink-cartridges',
+            MX360: '/pixma-mx360-ink-cartridges',
+            MX375: '/pixma-mx375-ink-cartridges',
+            MX395: '/pixma-mx395-ink-cartridges',
+            MX410: '/pixma-mx410-ink-cartridges',
+            MX420: '/pixma-mx420-ink-cartridges',
+            MX435: '/pixma-mx435-ink-cartridges',
+            MX455: '/pixma-mx455-ink-cartridges',
+            MX475: '/pixma-mx475-ink-cartridges',
+            MX495: '/pixma-mx495-ink-cartridges',
+            MX515: '/pixma-mx515-ink-cartridges',
+            MX525: '/pixma-mx525-ink-cartridges',
+            MX535: '/pixma-mx535-ink-cartridges',
+            MX700: '/pixma-mx700-ink-cartridges',
+            MX715: '/pixma-mx715-ink-cartridges',
+            MX725: '/pixma-mx725-ink-cartridges',
+            MX860: '/pixma-mx860-ink-cartridges',
+            MX870: '/pixma-mx870-ink-cartridges',
+            MX885: '/pixma-mx885-ink-cartridges',
+            MX895: '/pixma-mx895-ink-cartridges',
+            MX925: '/pixma-mx925-ink-cartridges',
             Other: '/ink-toner/f/3349056800/'
         },
         'PIXMA Pro series': {
@@ -224,7 +218,26 @@ MX920 series    PIXMA MX925 /pixma-mx925-ink-cartridges
 
     // Function to output the form on the listings page
     AWA.func.listingsForm = function() {
-        return '';
+
+        return '\
+<div class="awa-ink-finder -listings-form">\
+    <h2 class="header-9">'+AWA.var.copy[LANG].title+'</h2>\
+    <div class="awa-ink-finder-listings-wrap">\
+        <p>'+AWA.var.copy[LANG].intro.listings+'</p>\
+        <form>\
+            <select class="awa-ink-finder-range">\
+                <option value="0">'+AWA.var.copy[LANG].range+'</option>\
+                '+AWA.func.getOptions(AWA.data)+'\
+            </select>\
+            <select class="awa-ink-finder-model">\
+                <option value="0">'+AWA.var.copy[LANG].model+'</option>\
+            </select>\
+            <input type="submit" class="awa-ink-finder-submit" value="'+AWA.var.copy[LANG].button.listings+'">\
+        </form>\
+    </div>\
+</div>\
+ ';
+
     };
 
     // Function to output the form on the category page
@@ -236,16 +249,15 @@ MX920 series    PIXMA MX925 /pixma-mx925-ink-cartridges
     <p>'+AWA.var.copy[LANG].intro.cat+'</p>\
     <form>\
         <select class="awa-ink-finder-range">\
-            <option value="'+AWA.var.copy[LANG].range+'">'+AWA.var.copy[LANG].range+'</option>\
+            <option value="0">'+AWA.var.copy[LANG].range+'</option>\
             '+AWA.func.getOptions(AWA.data)+'\
         </select>\
         <select class="awa-ink-finder-model">\
-            <option value="'+AWA.var.copy[LANG].model+'">'+AWA.var.copy[LANG].model+'</option>\
+            <option value="0">'+AWA.var.copy[LANG].model+'</option>\
         </select>\
         <input type="submit" class="awa-ink-finder-submit" value="'+AWA.var.copy[LANG].button.cat+'">\
     </form>\
     <div class="awa-ink-link">\
-        <p>Or browse all inks:</p>\
         <a href="/search/s/priceDesc/f/3010492700/" class="catagory-stamp-img--anchor">\
             <img src="//i1.adis.ws/i/canon/ink&amp;paper_all_nav_image.png?w=95" pagespeed_url_hash="1376854407" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">\
             All Ink, Toner &amp; Paper\
@@ -270,8 +282,7 @@ MX920 series    PIXMA MX925 /pixma-mx925-ink-cartridges
             model = range[ AWA.var.modelSelect.val() ];
 
             if(model) {
-                alert('go to ink page: ' + model);
-                //document.location = model;
+                document.location = model;
                 return false;
             }
 
@@ -292,7 +303,21 @@ MX920 series    PIXMA MX925 /pixma-mx925-ink-cartridges
 
         var models = AWA.data[ AWA.var.rangeSelect.val() ];
 
-        AWA.var.modelSelect.html( AWA.func.getOptions(models) );
+        if(models) {
+
+            AWA.var.modelSelect.html(
+                '<option value="0">'+AWA.var.copy[LANG].model+'</option>' + 
+                AWA.func.getOptions(models)
+            );
+
+            // select the first model so the user can see this dropdown is now populated
+            AWA.var.modelSelect.find('option:eq(1)').prop('selected', 'selected');
+
+        } else {
+
+            AWA.var.modelSelect.html('<option value="0">'+AWA.var.copy[LANG].model+'</option>');
+
+        }
 
         // return false rather than e.preventDefault because Canon code has 
         // delegated a handler also
@@ -318,25 +343,205 @@ MX920 series    PIXMA MX925 /pixma-mx925-ink-cartridges
     // CSS
 
     AWA.css = ' \
+.awa-ink-finder + h2,\
+.gallery-category-stamp .layout-items-container {\
+    display: none;\
+}\
+.awa-ink-finder {\
+    position: relative;\
+    overflow: auto;\
+    padding-bottom: 2em;\
+}\
 .awa-ink-finder h2 {\
+    color: #c00;\
     text-align: left;\
 }\
-.awa-ink-finder h2 + p {\
+.awa-ink-finder.-listings-form h2 {\
+    font-size: 1.7em;\
+    padding-bottom: 10px;\
+}\
+.awa-ink-finder h2 + p, \
+.awa-ink-finder .awa-ink-finder-listings-wrap p {\
     text-align: left;\
+    color: #444;\
 }\
 .awa-ink-finder form,\
 .awa-ink-finder h2,\
-.awa-ink-finder h2 + p {\
-    width: 70%;\
+.awa-ink-finder h2 + p,\
+.awa-ink-finder .awa-ink-finder-listings-wrap p {\
+    width: 75%;\
     float: left;\
 }\
-.awa-ink-link {\
+.awa-ink-finder.-listings-form h2 {\
+    width: 100%;\
+    float: none;\
+}\
+.awa-ink-finder.-listings-form .awa-ink-finder-listings-wrap p {\
+    width: 25%;\
     float: left;\
-    width: 20%;\
+    font-size: 1.2em;\
+    line-height: 40px;\
+    text-align: center;\
+    margin: 0;\
+}\
+.awa-ink-finder.-listings-form form {\
+    width: 75%;\
+    float: left;\
 }\
 .awa-ink-finder select,\
 .awa-ink-finder input {\
-    width: 33%;\
+    width: 31.5%;\
+    margin: 0 3.5% 4px 0;\
+    font-size: 1.1em;\
+    float: left;\
+}\
+.awa-ink-finder.-listings-form select,\
+.awa-ink-finder.-listings-form input {\
+    width: 32%;\
+    margin: 0 1% 0 0;\
+}\
+.awa-ink-finder-listings-wrap {\
+    border: 1px solid #d9dbdc;\
+    overflow: auto;\
+    padding: 10px 0;\
+}\
+.awa-ink-link {\
+    position: absolute;\
+    top: 2em;\
+    right: 0;\
+    width: 25%;\
+}\
+.awa-ink-link p {\
+    color: #444;\
+    font-size: 1.4em;\
+}\
+.awa-ink-link img {\
+    display: block;\
+    margin: 0 auto 10px auto;\
+}\
+.awa-ink-link a {\
+    text-align: center;\
+    font-size: 1.1em;\
+    font-weight: bold;\
+    color: #333;\
+    display: block;\
+    width: 100%;\
+}\
+.awa-ink-finder .awa-ink-finder-submit {\
+    color: #fff;\
+    font-weight: bold;\
+    background: #077;\
+    border: 0;\
+    width: auto;\
+    padding-left: 40px;\
+    padding-right: 40px;\
+    margin-right: 0;\
+}\
+.awa-ink-finder.-listings-form .awa-ink-finder-submit {\
+    width: 32%;\
+    margin: 0 1% 0 0;\
+    padding-left: 0;\
+    padding-right: 0;\
+}\
+.awa-ink-finder-submit.-loading {\
+    background-image: url("//cdn.optimizely.com/img/2201792135/b9518b7121c3431c96b70cf84d35d96c.gif");\
+    background-position: 5px 5px;\
+    background-repeat: no-repeat;\
+}\
+.awa-ink-finder.-cat-form:after {\
+    position: absolute;\
+    top: 0;\
+    right: 22.5%;\
+    content: "";\
+    height: 90%;\
+    width: 1px;\
+    background: #D0D3D4;\
+}\
+@media screen and (max-width: 1199px) {\
+    .awa-ink-finder.-listings-form .awa-ink-finder-listings-wrap p {\
+        display: none;\
+    }\
+    .awa-ink-finder.-listings-form form {\
+        width: 100%;\
+        float: none;\
+    }\
+    .awa-ink-finder select {\
+        width: 31%;\
+        margin: 0 3% 4px 0;\
+    }\
+    .awa-ink-finder.-listings-form select,\
+    .awa-ink-finder.-listings-form input {\
+        margin: 0 0 0 1%;\
+        width: 32%;\
+    }\
+    .awa-ink-finder .awa-ink-finder-submit {\
+        width: 31%;\
+        margin: 0 0 4px 0;\
+        padding-left: 0;\
+        padding-right: 0;\
+    }\
+    .awa-ink-finder.-listings-form .awa-ink-finder-submit {\
+        width: 32%;\
+        margin: 0 0 0 1%;\
+    }\
+    .awa-ink-finder.-cat-form:after {\
+        right: 22.7%;\
+    }\
+}\
+@media screen and (max-width: 991px) {\
+    .awa-ink-finder select,\
+    .awa-ink-finder input {\
+        font-size: 1em;\
+    }\
+    .awa-ink-finder h2 {\
+        font-size: 1.7em;\
+    }\
+    .awa-ink-finder.-listings-form {\
+        clear: both;\
+    }\
+    .awa-ink-finder.-listings-form h2 {\
+        padding: 10px 0;\
+    }\
+    .awa-ink-finder.-cat-form:after {\
+        display: none;\
+    }\
+}\
+@media screen and (max-width: 750px) {\
+    .awa-ink-link {\
+        display: none;\
+    }\
+    .awa-ink-finder form,\
+    .awa-ink-finder h2,\
+    .awa-ink-finder h2 + p {\
+        width: 100%;\
+        float: none;\
+    }\
+    .awa-ink-finder {\
+        width: 95%;\
+        margin: 0 auto;\
+        padding-bottom: 0;\
+    }\
+    .awa-ink-finder.-listings-form {\
+        width: 100%;\
+        padding-bottom: 3em;\
+    }\
+}\
+@media screen and (max-width: 540px) {\
+    .awa-ink-finder select,\
+    .awa-ink-finder .awa-ink-finder-submit {\
+        width: 100%;\
+        margin-right: 0;\
+    }\
+    .awa-ink-finder-listings-wrap {\
+        border: 0;\
+        padding: 0;\
+    }\
+    .awa-ink-finder.-listings-form select,\
+    .awa-ink-finder.-listings-form input,\
+    .awa-ink-finder.-listings-form .awa-ink-finder-submit {\
+        width: 100%;\
+        margin: 0 0 4px 0;\
+    }\
 }\
  ';
 
@@ -350,7 +555,7 @@ MX920 series    PIXMA MX925 /pixma-mx925-ink-cartridges
 
     if(VARIATION === 1) {
         // Add form to listings if included in variation
-        $('.search-options').before( AWA.func.listingsForm() );
+        $('.search-options:eq(0)').before( AWA.func.listingsForm() );
     }
 
     // Cache jQuery objects
