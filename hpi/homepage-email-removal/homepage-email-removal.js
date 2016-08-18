c6b8d7b4bc180ae75c1ae832e5cd2dca4b077646
Jmoss <jonathan@castlegateit.co.uk>
2016-08-18 12:23:09 +0100

    Wrong approach taken initially.

--- hpi/homepage-email-removal/homepage-email-removal.js
+++ hpi/homepage-email-removal/homepage-email-removal.js
@@ -29,11 +29,39 @@
         },
 
         css: '\
+            .col-mn-offset-2half {\
+                margin-left: 21%;\
+            }\
         ',
 
         vars: {
-        }
+        },
 
+        unbind: function ()
+        {
+            $('.vrm-entry-form').unbind();
+            $('.input--email').unbind();
+        },
+
+        manipulate: function ()
+        {
+            var $form = $('.js-vrm-entry-form');
+            var $button = $('button', $form).clone();
+            $('.col-sm-5', $form).addClass('col-sm-7 col-mn-offset-2half').removeClass('col-sm-5 .col-mn-offset-2');
+            $('.input--vrm').addClass('input--button-join').after($button[0]);;
+            $('.input--email').parents('.col-mn-8').remove();
+        },
+
+        bind: function()
+        {
+            $('.input--vrm').on("change keyup", function (value)
+            {
+                var formattedVal = value.replace(/\s/g, "").toUpperCase();
+                var re = (/^([0-9]{3,5}|[0-9]{1,4}[A-Z]{1,3}|[A-Z]{1,3}[0-9]{1,4}|[A-Z]{2}[0-9]{6}|[A-Z][0-9]{5,6}|[A-Z]{3}[0-9]{1,3}[A-Z]|[A-Z][0-9]{1,3}[A-Z]{3}|[0-9]{3}[A-Z][0-9]{3}|[A-Z]{2}[0-9]{2}[A-Z]{3})$/);
+                return re.test(formattedVal);
+                console.log(this);
+            })
+        },
     };
 
     /**
@@ -42,28 +70,9 @@
 
     // Log it
     AWA.log('Removing Email Field');
-
-    $('.vrm-entry-form').unbind();
-    $('.input--email').unbind();
-
-    var $form = $('.js-vrm-entry-form');
-    var $button = $('button', $form).clone();
-
-    function reconfigure()
-    {
-        $(this).append($button[0]);
-        $('.input--email', this).parents('.col-mn-8').remove();
-    }
-
-    $('.input--vrm').on("change keyup", function (value)
-        {
-            var formattedVal = value.replace(/\s/g, "").toUpperCase();
-            var re = (/^([0-9]{3,5}|[0-9]{1,4}[A-Z]{1,3}|[A-Z]{1,3}[0-9]{1,4}|[A-Z]{2}[0-9]{6}|[A-Z][0-9]{5,6}|[A-Z]{3}[0-9]{1,3}[A-Z]|[A-Z][0-9]{1,3}[A-Z]{3}|[0-9]{3}[A-Z][0-9]{3}|[A-Z]{2}[0-9]{2}[A-Z]{3})$/);
-            return re.test(formattedVal);
-        }
-    );
-
-    $form.each(reconfigure);
+    AWA.unbind();
+    AWA.manipulate();
+    AWA.bind();
 
     // Append CSS
     $('head').append('<style type="text/css">'+AWA.css+'</style>');
