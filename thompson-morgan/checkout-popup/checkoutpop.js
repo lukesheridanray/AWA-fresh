0b038d4a2b914bbcf685e27ba79bf271677eb923
Luke Vaughan <luke.vaughan@awa-digital.cpm>
2016-09-01 15:31:50 -0400

    chekout popup layout

---
+++ thompson-morgan/checkout-popup/checkoutpop.js
@@ -0,0 +1,107 @@
+/******
+attempt at local storage credit card fix.
+******/
+//
+// CGIT Optimizely Boilerplate - version 0.1.4
+//
+
+// JSHint flags
+// jshint multistr: true
+// jshint jquery: true
+
+// Wrap the experiment code in an IIFE, this creates a local scope and allows us to
+// pass in jQuery to use as $. Other globals could be passed in if required.
+var exp = (function($) {
+
+// Initialise the experiment object
+var exp = {};
+
+// Wrapper for console.log, to prevent the exp breaking on browsers which don't
+// (always) have 'console' set (e.g. IE9)
+exp.log = function (str) {
+    if (typeof window.console !== 'undefined') {
+        console.log(str);
+    }
+};
+
+// Log the experiment, useful when multiple experiments are running
+exp.log('Experiment Checkout Popup- V1');
+
+// Condition
+// If we cannot rely on URL's to target the experiment (always preferred), we can use a unique CSS selector
+exp.condition = $('#headerBasket');
+
+// Check for a condition and return false if it has not been met
+if(exp.condition && !exp.condition.length) {
+    exp.log('Experiment failed a condition');
+    return false;
+}
+
+// Variables
+// Object containing variables, generally these would be strings or jQuery objects
+exp.vars = {};
+
+// Styles
+// String containing the CSS for the experiment
+exp.css = '';
+
+// Functions
+// Object containing functions, some helpful functions are included
+exp.func = {};
+
+// This function waits till a condition returns true
+exp.func.waitFor = function(condition, callback, timeout, keepAlive) {
+    timeout = timeout || 20000;
+    keepAlive = keepAlive || false;
+    var intervalTime = 50,
+        maxAttempts = timeout / intervalTime,
+        attempts = 0,
+        interval = setInterval(function() {
+            if (condition()) {
+                if (!keepAlive) {
+                    clearInterval(interval);
+                }
+                callback();
+            } else if (attempts > maxAttempts) {
+                clearInterval(interval);
+            }
+            attempts ++;
+        }, intervalTime);
+};
+
+
+// Init function
+// Called to run the actual experiment, DOM manipulation, event listeners, etc
+exp.init = function() {
+
+    // append styles to head
+    $('head').append('<style type="text/css">'+this.css+'</style>');
+
+/*---------------------------------------------------------------------------*/
+
+
+/* I first need to give an ID to the table */
+$('.basketPromotions').attr('id', 'awa-table-box');
+
+
+/***Checks to see if broweser supports LocalStorageAPI***/
+
+if (localStorage) {
+  // LocalStorage is supported!
+} else {
+  // No support. Use a fallback such as browser cookies or store on the server.
+}
+
+
+
+
+};
+// Run the experiment
+exp.init();
+
+// Return the experiment object so we can access it later if required
+return exp;
+
+// Close the IIFE, passing in jQuery and any other global variables as required
+// if jQuery is not already used on the site use optimizely.$ instead
+})(jQuery);
